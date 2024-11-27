1c79ba23e315a5e61bf4694851bcd2375306e7ff
Baris Balci <barisbalcimusic@gmail.com>
2024-11-27 16:49:27 +0100

    proxy server created

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+
+app.use(cors());
+
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: process.env.TARGET_API,
+    changeOrigin: true,
+    pathRewrite: {
+      "^/api": "",
+    },
+  })
+);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server listening on port ${PORT}`);
+});
