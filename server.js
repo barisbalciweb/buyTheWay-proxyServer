76987d29908560032baec7d429639e6b9b029f46
Baris Balci <barisbalcimusic@gmail.com>
2024-12-15 22:19:14 +0100

    update cors settings

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://btw.barisbalci.de",
+    credentials: true,
+  })
+);
 
 app.use(
   "/api",
