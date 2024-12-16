afd8c202fb6a77b780b52d81f444ea4e0b444d0d
Baris Balci <barisbalcimusic@gmail.com>
2024-12-16 11:17:04 +0100

    update cors and target api logic

--- server.js
+++ server.js
@@ -3,13 +3,20 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { createProxyMiddleware } from "http-proxy-middleware";
 
-const app = express();
-const PORT = process.env.PORT || 5000;
 dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+const nodeEnv = process.env.NODE_ENV;
+const clientURL =
+  nodeEnv === "prod" ? process.env.CLIENT_URL_PROD : process.env.CLIENT_URL_DEV;
+const apiURL =
+  nodeEnv === "prod" ? process.env.API_URL_PROD : process.env.API_URL_DEV;
 
 app.use(
   cors({
-    origin: "https://btw.barisbalci.de",
+    origin: clientURL,
     credentials: true,
   })
 );
@@ -17,41 +24,22 @@ app.use(
 app.use(
   "/api",
   createProxyMiddleware({
-    target: process.env.TARGET_API,
+    target: apiURL,
     changeOrigin: true,
     pathRewrite: {
       "^/api": "",
     },
-    onProxyRes: (proxyRes, req, res) => {
-      proxyRes.headers["Access-Control-Allow-Origin"] =
-        "https://btw.barisbalci.de";
-      proxyRes.headers["Access-Control-Allow-Credentials"] = "true";
-    },
-    onProxyReq: (proxyReq, req, res) => {
-      if (req.method === "OPTIONS") {
-        res.setHeader(
-          "Access-Control-Allow-Origin",
-          "https://btw.barisbalci.de"
-        );
-        res.setHeader("Access-Control-Allow-Credentials", "true");
-        res.setHeader(
-          "Access-Control-Allow-Methods",
-          "GET, POST, PUT, DELETE, OPTIONS"
-        );
-        res.setHeader(
-          "Access-Control-Allow-Headers",
-          "Content-Type, Authorization"
-        );
-        res.status(200).end();
+    onProxyReq: (proxyReq, req) => {
+      if (req.headers.cookie) {
+        proxyReq.setHeader("Cookie", req.headers.cookie);
+      }
+
+      if (req.headers.authorization) {
+        proxyReq.setHeader("Authorization", req.headers.authorization);
       }
     },
   })
 );
-
-// ONLY FOR TEST
-app.get("/", async (req, res) => {
-  res.send("Working!");
-});
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server listening on port ${PORT}`);
